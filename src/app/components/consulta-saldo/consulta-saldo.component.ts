a168ef8073bcb7b8306c9149e9a2bd3b939ea2d0
Rui Leite <ruipimentelleite@gmail.com>
2026-06-20 10:55:03 -0300

    Gera `ConsultaSaldoComponent`.
    - Comando:
      ng generate component components/consulta-saldo

---
+++ src/app/components/consulta-saldo/consulta-saldo.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-consulta-saldo',
+  standalone: true,
+  imports: [
+  ],
+  templateUrl: './consulta-saldo.component.html',
+  styleUrl: './consulta-saldo.component.css',
+})
+export class ConsultaSaldoComponent {
+
+}
