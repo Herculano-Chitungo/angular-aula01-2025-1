ac2880f738f108ac38a15497d131e8b3ca907661
Rui Leite <ruipimentelleite@gmail.com>
2026-06-20 10:25:47 -0300

    Gera model Cliente em branco.
    - Comando:
      ng generate class models/cliente

---
+++ src/app/models/cliente.spec.ts
@@ -0,0 +1,16 @@
+import { Cliente } from './cliente';
+
+describe('Cliente', () => {
+
+  let cliente: Cliente;
+
+  beforeEach(() => {
+    cliente = new Cliente(
+    );
+  });
+
+  it('should create an instance', () => {
+    expect(cliente).toBeTruthy();
+  });
+
+});
